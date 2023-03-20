8d2483f4327ea86f3a82bde464d49d1252508ca7
Amit R <aregmi@karglobal.com>
2023-03-20 15:49:53 -0400

    init

---
+++ SwiftySwift/SwiftySwift/SwiftySwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftySwiftApp.swift
+//  SwiftySwift
+//
+//  Created by Amit on 2023-03-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftySwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
